a3ccee1c51cdf83b59a5afc40c3e0db0c65e2661
Yingbo Ma <mayingbo5@gmail.com>
2021-10-25 01:21:53 -0400

    Add basic type generation

--- src/compactify.jl
+++ src/compactify.jl
@@ -24,18 +24,17 @@ end
 using Base.Meta: isexpr
 
 macro compactify(block)
-    _compactify(block)
+    _compactify(__module__, block)
     nothing
     #block
 end
 
-function _compactify(block)
+function _compactify(mod, block)
     isexpr(block, :block) || error("@compatify takes a block!")
     stmts = block.args
     hasabstract = false
     abstract2concrete = Dict() # abstract type name => ismutable, body, [(name, concete fields), ...]
     names = []
-    expr = Expr(:block)
     # notation: T means abstract name, S means concrete name
     for ex in stmts; ex isa LineNumberNode && continue
         if isexpr(ex, :macrocall) && ex.args[1] === Symbol("@abstract")
@@ -50,7 +49,6 @@ function _compactify(block)
                 abstract2concrete[T] = ismutable, struct_body, []
             end
         elseif isexpr(ex, :struct)
-            @info "" ex
             struct_body = ex
             ismutable, S, fields = struct_body.args
             isexpr(S, :(<:)) || error("$S must be a subtype of some @abstract type!")
@@ -66,26 +64,68 @@ function _compactify(block)
                 push!(abstract2concrete[T][end], (S, fields))
             end
         else
-            push!(expr.args, ex)
+            error("What is this? $ex")
         end
     end
-    @info "" abstract2concrete
 
     hasabstract || error("There must be at least one `@abstract`!")
-    #pushfirst!(expr.args, :(@enum $(concrete_names...)))
-    enumexpr = Expr(:block)
+
+    expr = Expr(:block)
 
     for (T, (ismutable, struct_body, Ss)) in pairs(abstract2concrete)
-        afields = filter(x->!(x isa LineNumberNode), struct_body.args[3].args)
-        common_fields = [isexpr(f, :(::)) ? f.args[1] : f for f in afields]
+        ex = Expr(:block)
+        enumtype = gensym("ENUM")
+        push!(ex.args, :(@enum $enumtype $(first.(Ss)...)))
+
+        # common fields in the abstract type T
+        common_fields = [
+                         isexpr(f, :(::)) ? f.args[1] : f
+                         for f in filter(x->!(x isa LineNumberNode), struct_body.args[3].args)
+                        ]
 
         compact_fields = []
-        for (S, cfields) in Ss, f in cfields
-            f, t = isexpr(f, :(::)) ? f.args : (f, :Any)
+        compact_types = []
 
-            @info "" afields cfields
+        S2fields = Dict{
+                        Symbol, # S: concrete type's name. We need this to later build getproperty
+                        Tuple{
+                              Vector{Symbol},Dict{Symbol,Any}
+                             }, # fieldnames, oldname => (newname, newtype=>oldtype)
+                       }()
+
+        for (S, cfields) in Ss
+            fields = Symbol[]
+            name_map = Dict{Symbol,Any}()
+            S2fields[S] = fields, name_map
+
+            for f in cfields
+                f, t = isexpr(f, :(::)) ? (f.args[1], getproperty(mod, f.args[2])) : (f, t) # field and type
+
+                push!(fields, f)
+                if isempty(compact_fields) ||
+                    !isconcretetype(t) ||
+                    (idx = findfirst(sizeof(t) <= sizeof(t′) for t′ in compact_types); idx === nothing)
+                    # add new fields
+                    #
+                    # initialize or we cannot optimize this case
+                    newname = gensym(f)
+                    push!(compact_fields, newname)
+                    push!(compact_types, t)
+                    name_map[f] = newname, t
+                else # reuse old field
+                    name_map[f] = compact_fields[idx], (compact_types[idx] => t)
+                end
+            end
         end
+
+        for (f, t) in zip(compact_fields, compact_types)
+            push!(struct_body.args[3].args, :($f::$t))
+        end
+        push!(ex.args, struct_body)
+
+        push!(expr.args, ex)
+        @info "" S2fields compact_fields compact_types
     end
 
-    @info "" expr
+    @show expr
 end
