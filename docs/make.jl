3ea1bfdccc380f54ae9a5b9695f38124f0cd90cf
Yingbo Ma <mayingbo5@gmail.com>
2021-10-25 01:24:18 -0400

    fix url

--- docs/make.jl
+++ docs/make.jl
@@ -6,11 +6,11 @@ DocMeta.setdocmeta!(Unityper, :DocTestSetup, :(using Unityper); recursive=true)
 makedocs(;
     modules=[Unityper],
     authors="Yingbo Ma <mayingbo5@gmail.com> and contributors",
-    repo="https://github.com/Yingbo Ma/Unityper.jl/blob/{commit}{path}#{line}",
+    repo="https://github.com/YingboMa/Unityper.jl/blob/{commit}{path}#{line}",
     sitename="Unityper.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://Yingbo Ma.github.io/Unityper.jl",
+        canonical="https://YingboMa.github.io/Unityper.jl",
         assets=String[],
     ),
     pages=[
@@ -19,6 +19,6 @@ makedocs(;
 )
 
 deploydocs(;
-    repo="github.com/Yingbo Ma/Unityper.jl",
+    repo="github.com/YingboMa/Unityper.jl",
     devbranch="master",
 )
