1ada3b80bb74b390f9fd77cdbd53b8484c764ab0
Documenter.jl <documenter@juliadocs.github.io>
2021-10-25 05:26:19 +0000

    build based on 3ea1bfd

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Unityper","category":"page"},{"location":"#Unityper","page":"Home","title":"Unityper","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Unityper.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Unityper]","category":"page"}]
+}
