b66279f2fe354e0e6e6c89a453bdd51b0814ac0e
Yingbo Ma <mayingbo5@gmail.com>
2021-10-28 16:17:52 -0400

    Inform compiler that the handling is exhaustive.

--- src/compactified.jl
+++ src/compactified.jl
@@ -1,8 +1,8 @@
 macro compactified(head, block)
-    _compactified(__module__, head, block; debug=true)
+    _compactified(__module__, head, block)
 end
 
-function _compactified(mod, head, block; debug=false)
+function _compactified(mod, head, block)
     @assert isexpr(head, :(::)) "The head must be in the form of `a::AT`!"
     @assert isexpr(block, :block) "There must be a block!"
     var, T = head.args
@@ -34,14 +34,26 @@ function _compactified(mod, head, block; debug=false)
         end
 
         uninitialized = ifold === expr
-        condition = islast ? true : Expr(:call, isa_type_fun, Val(T), Val(S), var)
-        ifnew = Expr(ifelse(uninitialized, :if, :elseif), condition, Expr(:block, lnn, s_body))
-        uninitialized ? push!(body, ifnew) : push!(ifold.args, ifnew)
-        ifold = ifnew
+        behavior = Expr(:block, lnn, s_body)
+        if islast
+            uninitialized ? push!(body, behavior) : push!(ifold.args, behavior)
+        else
+            condition = Expr(:call, isa_type_fun, Val(T), Val(S), var)
+            ifnew = Expr(ifelse(uninitialized, :if, :elseif), condition, behavior)
+            uninitialized ? push!(body, ifnew) : push!(ifold.args, ifnew)
+            ifold = ifnew
+        end
     end
     if !has_match_all
         unhandled = setdiff(Ss, seenS)
         @assert isempty(unhandled) "Non-exhaustive handling of $T: $(join(unhandled, ", ")) are not handled!"
     end
+    if !islast # inform compiler that the handling is exhaustive.
+        uninitialized = ifold === expr
+        behavior = :($throw_unreachable())
+        uninitialized ? push!(body, behavior) : push!(ifold.args, behavior)
+    end
     esc(expr)
 end
+
+@noinline throw_unreachable() = error("Unreachable reached. Please file an issue to Unityper.jl.")
